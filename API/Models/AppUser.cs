6dea9ddab6ef543614b830f2753051d2b4fdc3b6
GermanKos <kostiakovg@gmail.com>
2022-10-25 11:04:22 +0300

    Created an AppUser model

---
+++ API/Models/AppUser.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class AppUser
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string? Bio { get; set; }
+        public string? Interests { get; set; }
+        public string? Country { get; set; }
+        public string? Gender { get; set; }
+        public string? ProfilePicture { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
