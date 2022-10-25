13e678ad64c36925bb2f5cd065edac693317c02a
GermanKos <kostiakovg@gmail.com>
2022-10-25 11:58:56 +0300

    established db, connection to db, and first table

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+    }
+}
