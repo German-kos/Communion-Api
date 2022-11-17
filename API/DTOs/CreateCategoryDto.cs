d133a90784113d53eb262f11886b5dc11b854bbe
German <KostiakovG@Gmail.com>
2022-11-17 16:55:26 +0200

    trying to fix possible nulls

--- API/DTOs/CreateCategoryDto.cs
+++ API/DTOs/CreateCategoryDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,12 @@ namespace API.DTOs
 {
     public class CreateCategoryDto
     {
+        public CreateCategoryDto(string name, string info, IFormFile imageFile)
+        {
+            Name = name;
+            Info = info;
+            ImageFile = imageFile;
+        }
         public string Name { get; set; }
         public string Info { get; set; }
         public IFormFile ImageFile { get; set; }
